22e2cd39a62a25b64c4ca1a2175a9981b789c4d5
JacobRicher <52228855+JacobRicher@users.noreply.github.com>
2024-01-14 03:00:39 -0500

    Added Traffic Generation and Shub's working code

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import rb_allocation as rb_alloc
+import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
     # TODO: Generate info about users' traffic (data) requirements
-        # Name: 
+        # Name: Jacob Richer
     # TODO: Generate info about DU/RU power that are serving users
         # Name: 
     # TODO: Create another non-AI algorithm to allocate resources
@@ -15,8 +17,32 @@ def main():
         # Name: 
     # TODO: Put all lines graphs on same figure (grid)
         # Name: 
+        
+    # Example usage:
+    """
+    data_rate = 100  # replace with the actual value
+    bandwidth = 10  # replace with the actual value
+    constant_W = 2  # replace with the actual value
+    power_density_noise = 0.01  # replace with the actual value
+    constant_h = 3  # replace with the actual value
 
-    print(rb_alloc.round_robin(3, 10, {"1": 50, "2": 100, "3": 250}))
-
+    result = calculate_transmitted_power(data_rate, bandwidth, constant_W, power_density_noise, constant_h)
+    print( result)
+    """
+    
+    
+    # Example of Round Robin 
+    # print(rb_alloc.round_robin(3, 10, {"1": 50, "2": 100, "3": 250}))
+    
+    
+    # Example of Generating User Traffic
+    """
+    arr = rb_alloc.generateTraffic(maxUsers=10, maxDataSize=256, trafficStyle=3, totalrequests=100, dataRange=12)
+    data = [t[1] for t in arr]
+    plt.scatter(np.arange(1,101), data, marker='o')
+    plt.grid(True)
+    plt.show()
+    """
+    
 if __name__ == "__main__":
     main()
--- rb_allocation.py
+++ rb_allocation.py
@@ -1,4 +1,131 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
+
+def generateTraffic(
+        minUsers=1,                 # Minimum Number of Users to Generate
+        maxUsers=1,                 # Maximum Number of Users to Generate
+        trafficStyle=0,             # Defines the Distribution of Data      < There's a better way to do this
+        minDataSize=8,              # Minimum Number of Bits to Generate
+        maxDataSize=64,             # Maximum Number of Bits to Generate
+        totalrequests=10,           # Number of individual requests to generate
+        dataRange=12                # Fuzziness around a specific range (for linear and sin)
+    ):
+    """
+    By Jacob Richer
+    Function to generate user traffic
+    Returns an Array of tuples in the form (UserID, Data, Priority)
+    
+    UserID is based on maxUsers
+    Data is based on trafficStyle and min/maxDataSize
+    Priority is randomly generated between 1 and 5
+    
+    ex. [(1, 128, 2) < User 1 requests 128 bits of data with priority 2
+        (2, 176, 1)  < User 2 requests 176 bits of data with priority 1
+        (1, 64, 3)]  < User 1 requests 64 bits of data with priority 3
+    """
+    # Declarations
+    np.random.seed(3)
+    users_data_req = []
+    
+    # Switch into Traffic Style
+    match trafficStyle:
+        # Random Style - Completely Random Generation
+        case 0:
+            flag = True
+            if (minUsers >= maxUsers):
+                UserID=1
+                flag=False
+                
+            for i in range(1,totalrequests+1):
+                
+                if (flag):
+                    userID = np.random.randint(minUsers, maxUsers)
+                    
+                if (minDataSize < maxDataSize):
+                    dataRequirement = np.random.randint(minDataSize, maxDataSize)
+                else:
+                    dataRequirement = 0 
+                
+                priority = np.random.randint(1,5)
+                
+                users_data_req.append((userID, dataRequirement, priority))
+        
+        # Linear-Acending Style - Generate Traffic from min% data req to 100%
+        case 1:
+            scalingFactor = ((maxDataSize-dataRange)-(minDataSize+dataRange)) / (totalrequests-1)
+            offset = (minDataSize+dataRange) - scalingFactor
+            
+            flag = True
+            if (minUsers >= maxUsers):
+                UserID=1
+                flag=False
+            
+            for i in range(1,totalrequests+1):
+                scalingDataSize = (scalingFactor*i) + offset
+                
+                if (flag):
+                    userID = np.random.randint(minUsers, maxUsers)
+                    
+                dataRequirement = np.random.randint(scalingDataSize-dataRange, scalingDataSize+dataRange)
+                if not (minDataSize <= dataRequirement <= maxDataSize):
+                    dataRequirement = 0
+                
+                priority = np.random.randint(1,5)
+                
+                users_data_req.append((userID, dataRequirement, priority)) 
+        
+        # Linear-Descending Style - Generate Traffic from 100% to min%
+        case 2:
+            scalingFactor = ((minDataSize+dataRange)-(maxDataSize-dataRange)) / (totalrequests-1)
+            offset = (maxDataSize-dataRange) - scalingFactor
+            
+            flag = True
+            if (minUsers >= maxUsers):
+                UserID=1
+                flag=False
+                
+            for i in range(1,totalrequests+1):
+                scalingDataSize = (scalingFactor*i) + offset
+                
+                if (flag):
+                    userID = np.random.randint(minUsers, maxUsers)
+                    
+                dataRequirement = np.random.randint(scalingDataSize-dataRange, scalingDataSize+dataRange)
+                if not (minDataSize <= dataRequirement <= maxDataSize):
+                    dataRequirement = 0
+                
+                priority = np.random.randint(1,5)
+                
+                users_data_req.append((userID, dataRequirement, priority)) 
+        
+        # Sin-Wave Style - Generates a single sin wave spaning the number of elements requested at a single time
+        case 3:
+            amplitude = ((maxDataSize-dataRange)-(minDataSize+dataRange))/2
+            frequency = (2*np.pi)/(totalrequests-1)
+            vertical = amplitude + (minDataSize+dataRange)
+            
+            flag = True
+            if (minUsers >= maxUsers):
+                UserID=1
+                flag=False
+            
+            for i in range(1,totalrequests+1):
+                scalingDataSize = amplitude * np.sin(frequency * i) + vertical
+                
+                if (flag):
+                    userID = np.random.randint(minUsers, maxUsers)
+                
+                dataRequirement = np.random.randint(scalingDataSize-dataRange, scalingDataSize+dataRange)
+                if not (minDataSize <= dataRequirement <= maxDataSize):
+                    dataRequirement = 0
+                
+                priority = np.random.randint(1,5)
+                
+                users_data_req.append((userID, dataRequirement, priority)) 
+    
+    # Return The Array
+    return users_data_req
 
 def round_robin(channel_size, resource_element_size, users_data_req):
     """
@@ -93,9 +220,28 @@ def round_robin(channel_size, resource_element_size, users_data_req):
 #POWER VS TIME GRAPH CREATION CODE (I am still working on it ) - SHUB 
 #############################################################################
 ########### 1-time slots to the multiple time slots #####################
+def datarate(timeslots , req_data_amt):
+    datarate = 0
+    datarate = req_data_amt / timeslots
+    calculate_transmitted_power(datarate)
 
 
+def calculate_transmitted_power(data_rate):
+        """
+        Calculate transmitted power using the given formula.
 
+        Parameters:
+        - data_rate: Data rate (r)
+        - bandwidth: Bandwidth (w)
+        - constant_W: Constant term (W)
+        - power_density_noise: Power spectral density of noise (N0)
+        - constant_h: Another constant term (h)
+
+        Returns:
+        - Transmitted power (Pt)
+        """
+        transmitted_power = (2 ** (data_rate / bandwidth) - 1) * constant_W * power_density_noise / constant_h
+        return transmitted_power
 #############################################################################
 ########### multiple time slots to the powers time graph #########################
 
@@ -113,7 +259,14 @@ def powerConversion(array):
     power_per_timeslot = counter * Power_per_channel
     print(power_per_timeslot)
 # graphing power vs time but here we have 1    
+
 def GraphingPvsT():
+    pass
+
+
+
+
+
 
 def main():
     powerConversion(Timeslots)
