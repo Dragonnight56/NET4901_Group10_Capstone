e05896db60c8ffc3208b786f8de42ed600f2a9b5
JL <jacky.liang@cmail.carleton.ca>
2024-01-17 10:34:14 -0500

    Fixed indenting and rename second algorithm

--- rb_allocation.py
+++ rb_allocation.py
@@ -18,8 +18,8 @@ def channel_for_user(channels, symbols, resource_element_size, users_data_req):
     Assign user 1 to channel 1 (array 0)
     Assign user 2 to channel 2 (array 1)
     Assign user 3 to channel 3 (array 2)
-
     """
+
     arr = [[0 for i in range(symbols)] for j in range(channels)]
     queue = {}
 
@@ -46,65 +46,50 @@ def channel_for_user(channels, symbols, resource_element_size, users_data_req):
                     
     for row in arr:
         print(row)
+    
     print("This amount of data is left in the queue: " + str(queue))
 
-    """
-    ##Loop 3 times for each user, start with user 1 go to 2 and 3
-    ##Loop 3 times for each user, start with user 1 go to 2 and 3
-    for users, data in users_data_req.items():
-        i = 0
-        print("USER "+ users + " found")
-        ##While user 1 still has data, assign to channel 1
-        if int(users) > channels:
-            queue[users] = data
-        else:
-            while data > 0:
-                if(i > 9):
-                    print("Resource blocks full, queue extra data")
-                    queue[users] = data
-                    break
-                else:
-                    arr[int(users)-1][i] = int(users)
-                    data = data - resource_element_size
-                    i = i + 1
-
-    for row in arr:
-        print(row)
-    print("This amount of data is left in the queue: " + str(queue))
-
-    ##process_rb.process_rblock(arr, queue)
-    """
     return(arr)
 
 def fill_channels(channels, symbols, resource_element_size, users_data_req):
-    arr = [[0 for i in range(symbols)] for j in range(channels)]
+    # Initialize matrix with zeros
+    arr = [[0 for _ in range(symbols)] for _ in range(channels)]
+    # Create a copy of user data requests
     data_req = users_data_req.copy()
+    # Create cyclic iterator for user keys
     cyclic_keys = cycle(users_data_req.keys())
+    # Initialize user counters
     user_counters = {user_id: 0 for user_id in users_data_req}
-    while sum(data_req.values()) > 0:
-        arr = [0 for _ in range(symbols) for _ in range(channels)]
-        for k in range(channels):
-            print("print the k = " + str(k))
-            user_id = int(next(cyclic_keys))
 
-            while data_req[str(user_id)] == 0:
-                user_id = int(next(cyclic_keys))
+   # while sum(data_req.values()) > 0:
+    for k in range(channels):
+        # Reset matrix for the first channel
+        if k == 0:
+            arr = [[0 for _ in range(symbols)] for _ in range(channels)]
 
-            for i in range(symbols):
-
-             if data_req[str(user_id)] > 0:
-                print(f"USER {user_id} found")
+        # Get the next user_id from cyclic iterator
+        user_id = int(next(cyclic_keys))
+        for i in range(symbols):
+            if data_req[str(user_id)] > 0:
+                print(f"USER {user_id} FOUND")
                 data_req[str(user_id)] -= resource_element_size
                 user_counters[str(user_id)] += 1
-                print(f"Data for {user_id}: {data_req[str(user_id)]}")
-                arr[k][i] = user_id
+                print(f"Data req of {user_id}: {data_req[str(user_id)]}")
+                if data_req[str(user_id)] > 0:
+                    # if i > 0 :
+                    arr[k][i] = user_id
 
-             print(arr)
-             print(k)
-             if i < symbols - 1:
-              current_key = next(cyclic_keys)
-              user_id = (int(current_key))
-             
+            print(arr)
+            print("Number of resource elements that user " + str(user_id) + " has " +  str(user_counters[str(user_id)])  )
+            print("###############################################")
+            # print("Current row:", k)
+            # print("Current column:", i)
+            # Move to the next user_id only if there are more iterations
+            if i < symbols - 1:
+                current_key = next(cyclic_keys)
+                user_id = int(current_key)
+    
+    # Print the final matrix
     for row in arr:
         print(row)
 
