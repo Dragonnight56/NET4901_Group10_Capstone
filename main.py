8a938211e7121784d22b517eefaad23ac3e19cd6
Billy Bob <billybob@coolmathgames.com>
2024-03-21 16:49:46 -0400

    Added main simulation steps

--- main.py
+++ main.py
@@ -7,18 +7,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
-    '''
-    # Example calculating transmission power
-    print(cal.calc_transmit_power())
 
-    # Example calculate data rate
-    print(cal.calc_data_rate())
-    '''
-    
+    # --- Setup Simulation ---
     # Generating a New Plane
     plane = nodes.Plane(height=1000, width=1000)
     
     # Generating New Stations
+        # TODO: Setup inital transmission power
     stationArr = [nodes.Station(1, posX=500, posY=300, range=50),
                   nodes.Station(2, posX=500, posY=500, range=500),
                   nodes.Station(3, posX=750, posY=750, range=50),
@@ -33,47 +28,29 @@ def main():
     nodes.calculateAssociations(userArr, stationArr)
     
     # Create Traffic for all Users
+        # TODO: Change this to users send signal to (interference) noise ratio (SNR / SINR) it wants the station to target
+            # SNR is the received power (from station point of view)
     for station in stationArr:
         for user in station.users:
             # Each User is Given a Random Seed
             user.generateTraffic(np.random.randint(1,65536))
+
+    # TODO: Calculate loss between stations and users
     
+    # TODO: Calculate stations' transmission (signal) power (P_t) needed to meet target SNR
+        # P_t[dB] = target SNR[dB] + loss[dB]
+        # P_t[W] = target SNR * loss
+            
+    # TODO: After every sim tick, save calculated values into CSV file for model training
+            
+    # TODO: Feed calculated values into DL model
+    
+    # TODO: Have DL model gives recommendations for which cells to turn on and off (array of 0s and 1s)
+            
+    # TODO: Update topology using recommendations
     
     # Run Simulation
     graph.statSimulation(plane, stationArr, userArr, runTime=7200)
-    
-    '''     
-    # Pull User IDs and Traffic from all Stations
-    userTraffic = []
-    for station in stationArr:
-        for user in station.users:
-            userTraffic.append([user.userID, user.traffic])
-
-    # Print All Current Traffic
-    for user in userTraffic:
-        print(f"User {user[0]}: ")
-        for req in user[1]:
-            print(req, end="\t")
-        print()
-    
-    # Print Traffic for a specific Station
-    for user in stationArr[0].users:
-        print(f"{user.userID}: {user.traffic}")
-        
-    # Allocation Example
-    for station in stationArr:
-        trafficArr = []
-        
-        for user in station.users:1
-            trafficArr.append([user.userID, user.traffic])
-        
-        print(trafficArr)
-        rb_alloc.fill_channels(3,10,10,trafficArr)
-    '''
-    
-    # Display the Current Grid
-    # graph.graphPlane(plane, stationArr)
-    
     
 if __name__ == "__main__":
     main()
